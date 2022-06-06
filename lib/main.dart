941f2a142e2f9c8bd4c1915e8a161d79c8b03350
cembag <101637364+cembag@users.noreply.github.com>
2022-06-06 16:46:26 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
+// ignore_for_file: non_constant_identifier_names, prefer_typing_uninitialized_variables
 import 'dart:async';
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:development/Screens/loginView/home_view.dart';
 import 'package:development/Screens/onboard/onboards_view.dart';
-import 'package:development/controller/countrycode_controller.dart';
+import 'package:development/controller/project_controller.dart';
+import 'package:development/models/animatedsplashscreen.dart';
+import 'package:development/models/date.dart';
+import 'package:development/models/user.dart';
 import 'package:development/product/color/image_color.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-CountryCodeConrtroller controller = Get.put(CountryCodeConrtroller());
+final bool IS_COUNTRY_USA  = FirebaseRemoteConfig.instance.getBool('check_country');
+String? REVENUE_CAT_API_KEY;
+PurchaserInfo? purchaserInfo;
+Offerings? offerings; 
+Offering? offering;
+Package? packageAnnual;
+Package? packageWeekly;
+
+ProjectController controller = Get.put(ProjectController());
 var parsedJson;
 int? isViewed;
 void main() async {
@@ -27,16 +41,6 @@ void main() async {
     systemNavigationBarColor: Colors.transparent,
     systemNavigationBarDividerColor: Colors.transparent,
   ));
-
-  NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
-  alert: true,
-  announcement: true,
-  badge: false,
-  carPlay: false,
-  criticalAlert: false,
-  provisional: false,
-  sound: true,
-);
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(const BenimUyg());
@@ -58,16 +62,15 @@ class _BenimUygState extends State<BenimUyg> {
   @override
   initState(){
     super.initState();
-    initMessaging();
-    initializeRemoteConfig();
+    _initializePurchases();
+    _initMessaging();
+    _initializeRemoteConfig();
     _messaging.getToken().then((newToken){
       controller.notificationToken.value = newToken!;
     });
   }
 
-
-
-  Future<void> initializeRemoteConfig() async {
+  Future<void> _initializeRemoteConfig() async {
     final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
     await remoteConfig.setConfigSettings(RemoteConfigSettings(
     fetchTimeout: const Duration(minutes: 1),
@@ -78,7 +81,50 @@ class _BenimUygState extends State<BenimUyg> {
     parsedJson = jsonDecode(messages);
   }
 
-  Future<void> initMessaging() async {
+  Future<void> _initializePurchases () async {
+    REVENUE_CAT_API_KEY = 'appl_yRUfhCipEFhmWdewvvhCPWSQVVb';
+    /// ENABLE/DISABLE DEBUGS LOGS.
+    await Purchases.setDebugLogsEnabled(true);
+
+    /// SET API KEY.
+    await Purchases.setup(REVENUE_CAT_API_KEY!);
+
+    /// FETCH THE OFFERINGS INFO.
+    offerings = await Purchases.getOfferings();
+
+    purchaserInfo = await Purchases.getPurchaserInfo();
+
+    UserService(uid: '').addtoDatabase();
+    _checkSubscribe();
+
+    /// FETCH THE OFFERING ENTITLED PREMIUM PLAN 
+    offering = offerings!.getOffering('premiumaccessallproducts');
+
+    packageAnnual = offering!.getPackage('\$rc_annual');
+    packageWeekly = offering!.getPackage('\$rc_weekly');
+
+    if(!mounted) return;
+  }
+
+  Future<void> _checkSubscribe() async {
+
+    DocumentSnapshot _docRef = await FirebaseFirestore.instance.collection('Users').doc(purchaserInfo!.originalAppUserId).get();
+    if(_docRef.get('subscribe') == true){
+      Duration _diff = DateTime.parse(formattedDate(_docRef.get('expiration_date'))).difference(DateTime.now());
+    if(_diff.inSeconds.toInt() <= 0){
+      FirebaseFirestore.instance.collection('Users').doc(purchaserInfo!.originalAppUserId).update({
+        'subscribe': false,
+        'expiration_date': FieldValue.delete(),
+        'subscribe_type': FieldValue.delete(),
+      });
+        controller.isSubscribe.value = false;
+        }else{
+          controller.isSubscribe.value = true;
+        }
+      }else{controller.isSubscribe.value = false;}
+  }
+
+  Future<void> _initMessaging() async {
   var androidInit = const AndroidInitializationSettings('@drawable/splash');
   var iosInit = const IOSInitializationSettings();
   var initSettings = InitializationSettings(android: androidInit, iOS: iosInit);
@@ -107,7 +153,7 @@ class _BenimUygState extends State<BenimUyg> {
   FirebaseMessaging.instance.getInitialMessage().then((message) {
     if(message != null){
     FlutterLocalNotificationsPlugin().show(
-      message.notification.hashCode,
+      message.notification!.hashCode,
       message.notification!.title,
       message.notification!.body,
       generalNotificationDetails
@@ -117,10 +163,10 @@ class _BenimUygState extends State<BenimUyg> {
 
   ///when app runned out
   FirebaseMessaging.onMessage.listen((RemoteMessage message) { 
-    RemoteNotification? notification = message.notification;
+    RemoteNotification? notification = message.notification!;
     AndroidNotification? android = message.notification!.android;
 
-  if (notification != null && android != null) {
+  if (android != null) {
         flutterLocalNotificationsPlugin.show(
           notification.hashCode,
           notification.title,
@@ -131,28 +177,38 @@ class _BenimUygState extends State<BenimUyg> {
   });
 }
 
+  void load() async {
+    Future.delayed(const Duration(seconds: 3), (){
+        setState(() {
+  
+        });
+      });
+    }
   @override
   Widget build(BuildContext context) {
+  load();
   return GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-    textTheme: GoogleFonts.sourceSansProTextTheme(
-      Theme.of(context).textTheme,
+      brightness: Brightness.light,
+      appBarTheme: AppBarTheme(centerTitle: true,color: ProjectColors().themeColor, elevation: 2),
+      scaffoldBackgroundColor: ProjectColors().scaffoldBackgroundColor,
     ),
-    appBarTheme: AppBarTheme(centerTitle: true,color: ProjectColors().themeColor, elevation: 2),
-    scaffoldBackgroundColor: ProjectColors().scaffoldBackgroundColor,
+    darkTheme: ThemeData(
+      brightness: Brightness.light,
+      appBarTheme: AppBarTheme(centerTitle: true,color: ProjectColors().themeColor, elevation: 2),
+      scaffoldBackgroundColor: ProjectColors().scaffoldBackgroundColor,
     ),
-    home: const OnboardPageOne(),
-    //isViewed == 1 ? const PaymentPage() : const OnboardPageOne(),
+    home: parsedJson == null || offering == null
+    ? const SplashScreen()
+    : isViewed == 1 ? const HomePage() : const OnboardPageOne(),
     );
   }
 }
 Future<void> firebaseMessagingBackgroundHandler(message) async {
-    print('Handling a background Message ${message.messageId}');
-    print(message.notification!.title);
-    print(message.notification!.body);
+
     FlutterLocalNotificationsPlugin().show(
-    message.notification.hashCode,
+    message.notification!.hashCode,
     message.notification!.title,
     message.notification!.body,
 
@@ -167,4 +223,5 @@ Future<void> firebaseMessagingBackgroundHandler(message) async {
     channelShowBadge: false,
     ),
     iOS: IOSNotificationDetails())); 
+
   }
